e7288f4026c76abf6eb4f5ac7384c2765a655880
Pat Driscoll <patrick.driscoll@my.jcu.edu.au>
2016-08-14 16:30:19 +1000

    Add name printing program

--- oddName.py
+++ oddName.py
@@ -1,1 +1,9 @@
 """ Patrick Driscoll """
+name = input("Enter name: ")
+while not name:
+    name = input("Enter name: ")
+count = 0
+for char in name:
+    count += 1
+    if count % 2 == 0:
+        print(char)
